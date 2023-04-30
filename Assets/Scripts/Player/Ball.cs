bb366b6a8647ef1d1cc9ccc0dcc5417dc5b36634
Tarun-GameDev <annamtarun007@gmail.com>
2023-04-30 15:38:22 +0530

    Adding New Project
    adding project

---
+++ Assets/Scripts/Player/Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    
+    
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
